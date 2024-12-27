7fdd913eb75dc2162f28a9e363b2620fdc180140
Sara <salamasara949@gmail.com>
2024-12-27 14:27:29 +0100

    addition d'autre aspect de la modelisation

--- src/main/java/com/example/campaignmanager/Task.java
+++ src/main/java/com/example/campaignmanager/Task.java
@@ -2,6 +2,7 @@ package com.example.campaignmanager;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -15,11 +16,30 @@ public class Task {
     private TextField task_text;
 
     @FXML
-    private VBox tasks;
+    private VBox completedtasks;
+
+    @FXML
+    private VBox inProgress_task;
+
+    @FXML
+    private Button c_task_title;
 
     @FXML
     void add_new_task(ActionEvent event) {
 
     }
+    @FXML
+    private Button task_title;
+
+    @FXML
+    void complete_task(ActionEvent event) {
+
+    }
+
+    @FXML
+    void remove_task(ActionEvent event) {
+
+    }
+
 
 }
